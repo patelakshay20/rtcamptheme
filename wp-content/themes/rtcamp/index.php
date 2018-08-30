38dde254f61f348bf9ae99ef9b5721e6e9c64842
Patel Akshay <akupatel20@gmail.com>
2018-08-30 11:54:27 +0530

    Update index.php
    New Updated Index FILE

--- wp-content/themes/rtcamp/index.php
+++ wp-content/themes/rtcamp/index.php
@@ -4,9 +4,9 @@
     <head>
         <title><?php echo get_bloginfo( 'name' ); ?> - <?php echo get_bloginfo( 'description' ); ?></title>
         <meta charset="UTF-8">
-        <meta name="description" content="">
-        <meta name="keywords" content="">
-        <meta name="author" content="">
+        <meta name="description" content="<?php the_field('description'); ?>">
+        <meta name="keywords" content="<?php the_field('keywords'); ?>">
+        <meta name="author" content="<?php the_field('author'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" href="<?php echo $favicon_icon = ot_get_option( 'favicon_icon' ); ?>" type="image/gif" sizes="16x16">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
